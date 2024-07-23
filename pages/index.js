71091b0045c8b1d69f5d91d255a7344937000edf
HappyLeslieAlexander <HappyLeslieAlexander@duck.com>
2024-07-23 13:29:33 +0800

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,34 @@
+import { useEffect, useState } from 'react';
+
+export default function Home() {
+    const [data, setData] = useState({});
+    const [latency, setLatency] = useState(null);
+
+    useEffect(() => {
+        async function fetchData() {
+            const start = Date.now();
+            const token = process.env.NEXT_PUBLIC_IPINFO_TOKEN; // 使用环境变量
+            const response = await fetch(`https://ipinfo.io/json?token=${token}`);
+            const data = await response.json();
+            setLatency(Date.now() - start);
+            setData(data);
+        }
+
+        fetchData();
+    }, []);
+
+    return (
+        <div className="container">
+            <h1>IP Address Information</h1>
+            <p>Your IP: <span>{data.ip}</span></p>
+            <p>City: <span>{data.city}</span></p>
+            <p>Region (State): <span>{data.region}</span></p>
+            <p>Country: <span>{data.country}</span></p>
+            <p>Postal Code: <span>{data.postal}</span></p>
+            <p>Timezone: <span>{data.timezone}</span></p>
+            <p>Language: <span>{navigator.language || navigator.userLanguage}</span></p>
+            <p>User Agent: <span>{navigator.userAgent}</span></p>
+            <p>Latency: <span>{latency} ms</span></p>
+        </div>
+    );
+}
